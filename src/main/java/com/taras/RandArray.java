06421e6a51357d592bc6ed771ac1da0549bcbfda
taras-yaroshchuk <taras.yaroshchuk@gmail.com>
2015-11-04 20:21:14 +0200

    Fixed issue #3 'Generl issues about first task'

--- src/main/java/com/taras/RandArray.java
+++ src/main/java/com/taras/RandArray.java
@@ -1,6 +1,9 @@
 package com.taras;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
+
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
@@ -10,28 +13,34 @@ import org.apache.logging.log4j.LogManager;
 public class RandArray {
     public static final Logger log = LogManager.getLogger(RandArray.class);
 
-    private int[] array;
+    private int[] randomArray;
     private Random rand = new Random();
+    private Map<Integer, Integer> numbsWithCounters = new HashMap<>();
     private int[] numbs = {1, 3, 5, 7, 9};
-    private int[] counter = {0, 0, 0, 0, 0};
 
     private void init() {
-        for (int i = 0; i < array.length; i++) {
-            int temp = rand.nextInt(5);
-            array[i] = numbs[temp];
-            counter[temp]++;
-            log.trace("Number \"" + numbs[temp] + "\" was added " + counter[temp]
+        for (int i = 0; i < randomArray.length; i++) {
+            int temp = rand.nextInt(numbs.length);
+            randomArray[i] = numbs[temp];
+            numbsWithCounters.put(numbs[temp], numbsWithCounters.get(numbs[temp]) + 1);
+            log.trace("Number \"" + numbs[temp] + "\" was added " + numbsWithCounters.get(temp)
             + " times.");
+        }
+    }
+
+    {
+        for (int number : numbs) {
+            numbsWithCounters.put(number, 0);
         }
     }
 
     RandArray() {
 
-        array = new int[rand.nextInt()];
+        randomArray = new int[rand.nextInt()];
         init();
     }
     RandArray(int size) {
-        array = new int[size];
+        randomArray = new int[size];
         init();
     }
 
@@ -40,7 +49,7 @@ public class RandArray {
         StringBuilder res = new StringBuilder("Final result:" + separator);
         for (int i = 0; i < numbs.length; i++) {
             res.append("Number \"" + numbs[i] + "\" has been written "
-            + counter[i] + " times." + separator);
+            + numbsWithCounters.get(numbs[i]) + " times." + separator);
         }
         return res.toString();
     }
