2e7561024ca3edc9872c8c5b72f13e2a123efa7b
sidorow.artem <sidorow.artem@gmail.com>
2015-11-05 10:18:19 +0200

    Please - see comments in code.

--- src/main/java/com/taras/RandArray.java
+++ src/main/java/com/taras/RandArray.java
@@ -1,11 +1,11 @@
 package com.taras;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
-
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
 
 /**
  * Created by taras on 26.10.2015.
@@ -20,14 +20,19 @@ public class RandArray {
 
     private void init() {
         for (int i = 0; i < randomArray.length; i++) {
-            int temp = rand.nextInt(numbs.length);
-            randomArray[i] = numbs[temp];
-            numbsWithCounters.put(numbs[temp], numbsWithCounters.get(numbs[temp]) + 1);
-            log.trace("Number \"" + numbs[temp] + "\" was added " + numbsWithCounters.get(temp)
-            + " times.");
+            int randomIndex = rand.nextInt(numbs.length);
+            //if you are using variable more than once - it's better to make a separate link
+            int value = numbs[randomIndex];
+            randomArray[i] = value;
+            numbsWithCounters.put(value, numbsWithCounters.get(value) + 1);
+            //small hint - logger CAN use pattern :-)
+            log.trace("Number \"{}\" was added {} times.", value, numbsWithCounters.get(value));
         }
     }
 
+    /*It's VERY bad practice to use static code blocks.
+     *Please, make code refactoring and surround this block with a method call.
+     **/
     {
         for (int number : numbs) {
             numbsWithCounters.put(number, 0);
@@ -47,9 +52,27 @@ public class RandArray {
     public String getResults() {
         String separator = System.getProperty("line.separator");
         StringBuilder res = new StringBuilder("Final result:" + separator);
+        /*
         for (int i = 0; i < numbs.length; i++) {
-            res.append("Number \"" + numbs[i] + "\" has been written "
-            + numbsWithCounters.get(numbs[i]) + " times." + separator);
+            //if you are using variable more than once - it's better to make a separate link
+            int key = numbs[i];
+            //Use StringBuilder this way - it's much better. Direct string concatenation is much more slow.
+            res.append("Number \"")
+                    .append(key)
+                    .append("\" has been written ")
+                    .append(numbsWithCounters.get(key))
+                    .append(" times.")
+                    .append(separator);
+        }
+        */
+        //Iterate over set - it's a bit more powerful, in case you don't need current index. Please read about Iterator class.
+        for(Integer key: numbsWithCounters.keySet()){
+            res.append("Number \"")
+                    .append(key)
+                    .append("\" has been written ")
+                    .append(numbsWithCounters.get(key))
+                    .append(" times.")
+                    .append(separator);
         }
         return res.toString();
     }
